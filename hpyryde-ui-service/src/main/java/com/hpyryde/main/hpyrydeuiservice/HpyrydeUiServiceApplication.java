642ddbfd1c50760a64f0daf36796b2365b333e3a
Manoj Ramakrishnan <manoj.rgv@gmail.com>
2022-05-09 19:51:35 +0530

    ui service folder added

---
+++ hpyryde-ui-service/src/main/java/com/hpyryde/main/hpyrydeuiservice/HpyrydeUiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hpyryde.main.hpyrydeuiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpyrydeUiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpyrydeUiServiceApplication.class, args);
+	}
+
+}
---
+++ hpyryde-ui-service/src/test/java/com/hpyryde/main/hpyrydeuiservice/HpyrydeUiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hpyryde.main.hpyrydeuiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpyrydeUiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

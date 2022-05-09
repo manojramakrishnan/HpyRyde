0f93570a89d46bf7747ee3b0d47b042c35597cee
Manoj Ramakrishnan <manoj.rgv@gmail.com>
2022-05-09 20:00:24 +0530

    ingestion service folder  added

---
+++ hpyryde-location-ingest-service/src/main/java/com/hpyryde/main/HpyrydeLocationIngestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hpyryde.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpyrydeLocationIngestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpyrydeLocationIngestServiceApplication.class, args);
+	}
+
+}

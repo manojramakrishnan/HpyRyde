ddf51fab0ff1045dda778468465ae9f7f9f6d057
Manoj Ramakrishnan <manoj.rgv@gmail.com>
2022-05-09 20:30:07 +0530

    hpyryde pickup  service folder added

---
+++ hpyryde-pickup-service/src/main/java/com/hpyryde/main/HpyrydePickupServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hpyryde.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpyrydePickupServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpyrydePickupServiceApplication.class, args);
+	}
+
+}

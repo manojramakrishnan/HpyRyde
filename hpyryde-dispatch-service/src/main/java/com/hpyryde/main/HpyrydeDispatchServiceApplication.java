7f2c0c5a17ae79d1b5163fab218de1fecb4780b3
Manoj Ramakrishnan <manoj.rgv@gmail.com>
2022-05-09 20:25:48 +0530

    hpyryde dispatch service folder added

---
+++ hpyryde-dispatch-service/src/main/java/com/hpyryde/main/HpyrydeDispatchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hpyryde.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpyrydeDispatchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpyrydeDispatchServiceApplication.class, args);
+	}
+
+}
---
+++ hpyryde-dispatch-service/src/test/java/com/hpyryde/main/HpyrydeDispatchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hpyryde.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpyrydeDispatchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

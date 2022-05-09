f61766fc02aaf5d09c4db2903cd7bc25d95ac7d9
Manoj Ramakrishnan <manoj.rgv@gmail.com>
2022-05-09 20:07:03 +0530

    location simulator service folder  added

---
+++ hpyryde-location-simulator-service/src/main/java/com/hpyryde/main/HpyrydeLocationSimulatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hpyryde.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpyrydeLocationSimulatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpyrydeLocationSimulatorServiceApplication.class, args);
+	}
+
+}

2fd49ab3c6a1a2a5a4e78c72ad63182d56d0c87c
Manoj Ramakrishnan <manoj.rgv@gmail.com>
2022-05-09 20:20:10 +0530

    eureka discovery server folder added

---
+++ hpyryde-discovery-server/src/main/java/com/hpyryde/main/HpyrydeDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.hpyryde.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpyrydeDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpyrydeDiscoveryServerApplication.class, args);
+	}
+
+}
---
+++ hpyryde-discovery-server/src/test/java/com/hpyryde/main/HpyrydeDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hpyryde.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpyrydeDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

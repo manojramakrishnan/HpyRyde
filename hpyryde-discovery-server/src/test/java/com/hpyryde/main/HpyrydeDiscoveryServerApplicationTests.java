6b5c54224816141dcd7a045f4b8de1d5caab8ccd
Manoj Ramakrishnan <manoj.rgv@gmail.com>
2022-05-12 01:35:05 +0530

    Build related issues fixed

--- hpyryde-discovery-server/src/test/java/com/hpyryde/main/HpyrydeDiscoveryServerApplicationTests.java
+++ hpyryde-discovery-server/src/test/java/com/hpyryde/main/HpyrydeDiscoveryServerApplicationTests.java
@@ -1,13 +1,9 @@
 package com.hpyryde.main;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+
 class HpyrydeDiscoveryServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
--- hpyryde-location-ingest-service/src/test/java/com/hpyryde/main/HpyrydeLocationIngestServiceApplicationTests.java
+++ hpyryde-location-ingest-service/src/test/java/com/hpyryde/main/HpyrydeLocationIngestServiceApplicationTests.java
@@ -1,13 +1,8 @@
 package com.hpyryde.main;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class HpyrydeLocationIngestServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }

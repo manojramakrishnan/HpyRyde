1347d59db05bbde15dbff9947716eb4ce4c691db
Naveen <naveen25navics@gmail.com>
2022-05-11 21:14:47 +0530

    eureka server setup

--- hpyryde-ui-service/src/main/java/com/hpyryde/main/hpyrydeuiservice/HpyrydeUiServiceApplication.java
+++ hpyryde-ui-service/src/main/java/com/hpyryde/main/hpyrydeuiservice/HpyrydeUiServiceApplication.java
@@ -2,8 +2,10 @@ package com.hpyryde.main.hpyrydeuiservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class HpyrydeUiServiceApplication {
 
 	public static void main(String[] args) {

dc6829f83bbe6173830e22b9abaccb8bdff0b4d8
akhil murali <akhilmurali662@gmail.com>
2022-06-07 15:54:51 +0530

    added model

---
+++ hpyryde-location-intimation-service/src/main/java/com/hpyryde/main/model/VehicleStatus.java
@@ -0,0 +1,10 @@
+package com.hpyryde.main.model;
+
+
+
+
+public enum VehicleStatus {
+	
+	
+	NONE, SERVICE_INFO, SERVICE_SOON, SERVICE_NOW, STOP_NOW
+}
---
+++ hpyryde-location-intimation-service/src/main/java/com/hpyryde/main/web/WebSocketApi.java
@@ -0,0 +1,19 @@
+package com.hpyryde.main.web;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ *
+ */
+@RestController
+public class WebSocketApi {
+
+	@MessageMapping("/sendMessage")
+	@SendTo("/queue/vehicles")
+	public String sendMessage(String message) throws Exception {
+		return message;
+	}
+}
